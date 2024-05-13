1a9a2c46a58f72ede8a141c51fee9d8ed40a836d
sinsohi <136775478+sinsohi@users.noreply.github.com>
2024-05-14 01:32:27 +0900

    Title: 특정 조건 두 정수 비교, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240514/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
+++ 240514/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.cpp
@@ -5,7 +5,7 @@ int main() {
     int a,b;
     cin >> a >> b;
     if(a < b) cout << "1";
-    else if (a > b) cout << "0";
+    else cout << "0";
     cout << " ";
     if(a==b) cout << "1";
     else cout << "0";
