8c403cfae11b9ee9adb29299b3f924942f039a19
sinsohi <136775478+sinsohi@users.noreply.github.com>
2024-05-14 00:51:41 +0900

    Title: 합과 평균, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240514/합과 평균/sum-and-avg.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b;
+    cin >> a >> b;
+    cout << a+b;
+    cout << fixed;
+    cout.precision(1);
+    cout << " " << (double)(a+b)/2;
+    return 0;
+}
